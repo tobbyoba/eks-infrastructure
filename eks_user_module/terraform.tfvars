b8aace995c98456fec08c8fded902c54dd3fb100
tobbyoba <tobbyoba86@gmail.com>
2023-10-22 18:25:10 -0500

    module updated

---
+++ eks_user_module/terraform.tfvars
@@ -0,0 +1,5 @@
+env = ["Development", "Production"]
+username = ["developer1", "manager", "mark"]
+tags = {
+  "Env" = "Production"
+}
---
+++ eks_user_module/variables.tf
@@ -0,0 +1,12 @@
+variable "username" {
+  type    = list(any)
+}
+
+variable "env" {
+  type    = list(any)
+}
+
+variable "tags" {
+  type = map(string)  
+}
+
